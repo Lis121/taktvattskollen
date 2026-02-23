6fca5c5dee26539d3f274396fd20823d9f02579f
Lis121 <leo00lis@gmail.com>
2026-02-23 10:15:12 +0100

    feat: add Microsoft Clarity tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,8 +37,8 @@ export default function RootLayout({
         {
           "@type": "Offer",
           "itemOffered": {
-             "@type": "Service",
-             "name": "Algbehandling av tak"
+            "@type": "Service",
+            "name": "Algbehandling av tak"
           }
         }
       ]
@@ -48,6 +48,18 @@ export default function RootLayout({
   return (
     <html lang="sv">
       <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "vlsuiryrl6");
+            `
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
